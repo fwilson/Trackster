28d9a2c35810a1ae45c7beabd8819901d0d2f91b
fwilson <fwilsonjr@gmail.com>
2017-05-05 14:41:59 -0400

    adding search button functionality via jquery

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,17 +1,27 @@
-var Trackster = {};
+$(document).ready(function () {
 
-/*
-  Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
-*/
-Trackster.renderTracks = function(tracks) {
+  var Trackster = {};
 
-};
+  /*Search Button Click Functionality*/
 
-/*
-  Given a search term as a string, query the Spotify API.
-  Render the tracks given in the API query response.
-*/
-Trackster.searchTracksByTitle = function(title) {
+  $('.search-btn').click(function () {
+    console.log($('.search-input').val());
+  })
 
-};
+  /*
+    Given an array of track data, create the HTML for a Bootstrap row for each.
+    Append each "row" to the container in the body to display all tracks.
+  */
+  Trackster.renderTracks = function(tracks) {
+
+  };
+
+  /*
+    Given a search term as a string, query the Spotify API.
+    Render the tracks given in the API query response.
+  */
+  Trackster.searchTracksByTitle = function(title) {
+
+  };
+
+})

5c4e50499e65111c5b6b467cfc27479166482901
fwilson <fwilsonjr@gmail.com>
2017-05-05 15:25:19 -0400

    finished search functionality

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -2,12 +2,6 @@ $(document).ready(function () {
 
   var Trackster = {};
 
-  /*Search Button Click Functionality*/
-
-  $('.search-btn').click(function () {
-    console.log($('.search-input').val());
-  })
-
   /*
     Given an array of track data, create the HTML for a Bootstrap row for each.
     Append each "row" to the container in the body to display all tracks.
@@ -21,7 +15,21 @@ $(document).ready(function () {
     Render the tracks given in the API query response.
   */
   Trackster.searchTracksByTitle = function(title) {
+    var url = 'https://api.spotify.com/v1/search?type=track&q='+title;
+    $.ajax({
+      url: url,
+      datatype: 'jsonp',
+      success: function(data) {
+        console.log(data);
+      }
+    })
 
   };
+
+  /*Search Button Click Functionality*/
+
+  $('.search-btn').click(function () {
+    Trackster.searchTracksByTitle($('.search-input').val());
+  })
 
 })

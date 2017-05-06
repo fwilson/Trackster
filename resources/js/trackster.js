de85f402bae2dc972264995e81ab4d5e33e4e09a
fwilson <fwilsonjr@gmail.com>
2017-05-05 22:13:42 -0400

    update

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -7,18 +7,18 @@ $(document).ready(function () {
     Append each "row" to the container in the body to display all tracks.
   */
   Trackster.renderTracks = function(tracks) {
-      for ( var i = 0; i = tracks.length; i++) {
+      for (var i = 0; i = tracks.length; i++) {
         var track = tracks[i];
         var trackData =
           '<div class="row result">' +
             '<div class="col-xs-2 play-track">' +
               '<a href="' + track.preview_url + '"><i class="fa fa-play-circle-o fa-lg play-btn"></i></a>' +
-              '<span>'+ track.track_number +'"</span>' +
+              '<span>'+ track.track_number +'</span>' +
             '</div>' +
-            '<div class="col-xs-4 title">'+ track.name +'"</div>' +
-            '<div class="col-xs-2 artist">'+ track.artists[0].name +'"</div>' +
-            '<div class="col-xs-2 album">'+ track.album.name +'"</div>' +
-            '<div class="col-xs-2 popularity">'+ track.popularity +'"</div>' +
+            '<div class="col-xs-4 title">'+ track.name +'</div>' +
+            '<div class="col-xs-2 artist">'+ track.artists[0].name +'</div>' +
+            '<div class="col-xs-2 album">'+ track.album.name +'</div>' +
+            '<div class="col-xs-2 popularity">'+ track.popularity +'</div>' +
           '</div>';
         $('.results').append(trackData);
       }
